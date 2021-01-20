9c94195fd7253c0af891cc42325d0222c8bf2c5b
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-01-20 19:43:29 -0300

    Concert Index Created

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,2 +1,29 @@
 class ConcertsController < ApplicationController
+
+    def index
+        @concerts = Concert.all
+    end
+
+    def show
+    end
+
+    def new
+        @concert = Concert.new
+    end
+
+    def edit
+    end
+
+    def create
+        @concert = Concert.new(concert_params)
+    end
+
+    def destroy
+        @concert.destroy
+    end
+
+    private
+    def concert_params
+        params.require(:concert).permit(:participants, :duration, :concert_date, :Group_id)
+    end
 end
