42a280925608630bf62b00273655f023b27a838c
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-01-22 11:47:56 -0300

    now we show the concerts of this month in Group/show

--- app/models/group.rb
+++ app/models/group.rb
@@ -23,5 +23,15 @@ class Group < ApplicationRecord
         asistants.sum
     end
 
-   
+    def concerts_this_month
+        @concerts = Concert.where(Group_id: id)
+        concerts_month = []
+        @concerts.each do |concert|
+            if concert.concert_date > Time.now.beginning_of_month && concert.concert_date < Time.now.end_of_month
+                concerts_month.push(concert.concert_date)
+            end
+        end
+        concerts_month.count
+    end
+
 end
